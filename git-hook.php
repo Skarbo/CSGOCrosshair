52958a7ba33e81a1fa8593366ca1f9e8b77157cc
Kris Skarbo <kris.skarbo@gmail.com>
2013-02-22 11:40:44 +0100

    Updated git hook

--- git-hook.php
+++ git-hook.php
@@ -4,6 +4,45 @@ error_reporting( E_ALL );
 ini_set( 'display_errors', '1' );
 set_time_limit( 0 );
 
+function myErrorHandler( $errno, $errstr, $errfile, $errline )
+{
+    if ( !( error_reporting() & $errno ) )
+    {
+        // This error code is not included in error_reporting
+        return;
+    }
+
+    $errorText = array ();
+
+    switch ( $errno )
+    {
+        case E_USER_ERROR :
+            $errorText[] = "<b>My ERROR</b> [$errno] $errstr<br />\n";
+            $errorText[] = "  Fatal error on line $errline in file $errfile";
+            $errorText[] = ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />\n";
+            $errorText[] = "Aborting...<br />\n";
+            break;
+
+        case E_USER_WARNING :
+            $errorText[] = "<b>My WARNING</b> [$errno] $errstr<br />\n";
+            break;
+
+        case E_USER_NOTICE :
+            $errorText[] = "<b>My NOTICE</b> [$errno] $errstr<br />\n";
+            break;
+
+        default :
+            $errorText[] = "Unknown error type: [$errno] $errstr<br />\n";
+            break;
+    }
+
+    file_put_contents( 'logs/error.txt', $errorText, FILE_APPEND );
+
+    return true;
+}
+
+set_error_handler( "myErrorHandler" );
+
 try
 {
 
